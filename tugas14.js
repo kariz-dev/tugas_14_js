e532b3162c321812312874057a2d1f9d8a21e2dd
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-19 00:04:08 +0700

    tugas 14 js, Muhammad Rizwan

---
+++ tugas14.js
@@ -0,0 +1,16 @@
+/* Tugas 14 Javascript */
+// Muhammad Rizwan
+
+function tugasEmpatBelas(){
+    var pembagian = 37/4;
+
+
+    console.log('nilai awal:',pembagian);
+
+    console.log('hasil pembulatan ceil:',Math.ceil(pembagian));
+    console.log('hasil pembulatan floor:',Math.floor(pembagian));
+    console.log('hasil pembulatan round:',Math.round(pembagian));
+
+}
+
+tugasEmpatBelas();
